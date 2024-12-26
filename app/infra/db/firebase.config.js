04d4e7646683a72ce0c54355486af2765c06d8af
leandroperego <leperego@hotmail.com>
2024-12-26 18:56:59 -0300

    config variaveis ambiente

--- app/infra/db/firebase.config.js
+++ app/infra/db/firebase.config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_APIKEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECTID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
-  appId: process.env.NEXT_PUBLIC_APPID
+  apiKey: process.env.FIREBASE_PUBLIC_APIKEY,
+  authDomain: process.env.FIREBASE_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PUBLIC_PROJECTID,
+  storageBucket: process.env.FIREBASE_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_PUBLIC_APPID
 };
 
 // Initialize Firebase
